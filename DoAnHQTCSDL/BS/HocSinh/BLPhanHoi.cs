18e93e1a250f00e96d69ac9f27a59bb5c027d582
Tuấn Nguyễn <121430735+TuanNguyen238@users.noreply.github.com>
2023-11-05 11:13:59 +0700

    Update BLPhanHoi.cs

--- DoAnHQTCSDL/BS/HocSinh/BLPhanHoi.cs
+++ DoAnHQTCSDL/BS/HocSinh/BLPhanHoi.cs
@@ -11,7 +11,7 @@ namespace DoAnHQTCSDL.BS.HocSinh
 {
     internal class BLPhanHoi
     {
-        string maHS = "";
+        private string maHS = "";
         DBMain db = null;
         public BLPhanHoi(string maHs, DBMain db)
         {
